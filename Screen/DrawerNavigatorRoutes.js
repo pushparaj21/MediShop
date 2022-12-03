5b5b67242b1e552e1816edb4554f0fa26a713161
pushparaj21 <singhpushparaj31@gmail.com>
2022-12-04 00:17:49 +0530

    logout feature are succesfully implimented

--- Screen/DrawerNavigatorRoutes.js
+++ Screen/DrawerNavigatorRoutes.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {signOut} from 'firebase/auth';
+import {auth} from '../firebase/firebase-config';
 
-import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 function Feed() {
   return (
@@ -18,6 +19,30 @@ function Article() {
     </View>
   );
 }
+function Logout({navigation}) {
+  const signout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        console.log('signout successful');
+        navigation.navigate('Auth');
+      })
+      .catch(error => {
+        // An error happened.
+        console.log(error);
+      });
+  };
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        activeOpacity={0.5}
+        onPress={signout}>
+        <Text style={styles.buttonTextStyle}>LOGOUT</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 const Drawer = createDrawerNavigator();
 
@@ -26,7 +51,29 @@ function DrowerNavigatorRoutes() {
     <Drawer.Navigator useLegacyImplementation>
       <Drawer.Screen name="Feed" component={Feed} />
       <Drawer.Screen name="Article" component={Article} />
+      <Drawer.Screen name="Logout" component={Logout} />
     </Drawer.Navigator>
   );
 }
 export default DrowerNavigatorRoutes;
+const styles = StyleSheet.create({
+  buttonStyle: {
+    backgroundColor: '#7DE24E',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
+    width: '25%',
+  },
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+});
