85f77471d5215828fe88d48deeb898331ed3bb44
pushparaj21 <singhpushparaj31@gmail.com>
2022-12-16 12:31:51 +0530

    now we can find location and register form are ready every thing is fine

--- Screen/DrawerScreens/RegisterShop.js
+++ Screen/DrawerScreens/RegisterShop.js
@@ -1,10 +1,147 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-function Register() {
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  ScrollView,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+
+export default function Register() {
+  const [name, setName] = useState('');
+  const [shopName, setShopName] = useState('');
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+  const [contact, setContact] = useState();
+  const signInUser = () => {
+    console.log(`${latitude}  ${longitude}  ${contact}  ${name} ,${shopName}`);
+  };
+  Geolocation.getCurrentPosition(data => {
+    setLatitude(data.coords.latitude);
+    setLongitude(data.coords.longitude);
+    console.warn(`latitude ${latitude} and longitude ${longitude}`);
+  });
+  // console.warn(Geolocation);
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Article Screen</Text>
+    <View style={styles.mainBody}>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        // eslint-disable-next-line react-native/no-inline-styles
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}>
+        <KeyboardAvoidingView enabled>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={ShopName => setShopName(ShopName)}
+              placeholder="Enter Shop Title" //dummy@abc.com
+              placeholderTextColor="#8b9cb5"
+              autoCapitalize="none"
+              keyboardType="default"
+              returnKeyType="next"
+              underlineColorAndroid="#f000"
+              blurOnSubmit={false}
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={UserName => setName(UserName)}
+              placeholder="Your Name" //12345
+              placeholderTextColor="#8b9cb5"
+              keyboardType="default"
+              // onSubmitEditing={Keyboard.dismiss}
+              blurOnSubmit={false}
+              secureTextEntry={true}
+              underlineColorAndroid="#f000"
+              returnKeyType="next"
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={Contact => setContact(Contact)}
+              placeholder="Contact No" //12345
+              placeholderTextColor="#8b9cb5"
+              keyboardType="number"
+              // onSubmitEditing={Keyboard.dismiss}
+              blurOnSubmit={false}
+              secureTextEntry={true}
+              underlineColorAndroid="#f000"
+              returnKeyType="next"
+            />
+          </View>
+
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            activeOpacity={0.5}
+            onPress={signInUser}>
+            <Text style={styles.buttonTextStyle}>Register Shop</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </ScrollView>
     </View>
   );
 }
-export default Register;
+const styles = StyleSheet.create({
+  mainBody: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#307ecc',
+    alignContent: 'center',
+  },
+  SectionStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: '#7DE24E',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
+  },
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  inputStyle: {
+    flex: 1,
+    color: 'white',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: '#dadae8',
+  },
+  registerTextStyle: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+    alignSelf: 'center',
+    padding: 10,
+  },
+  errorTextStyle: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 14,
+  },
+});
